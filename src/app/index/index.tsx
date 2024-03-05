cf763b21cbcd8119598e7c9bae83a14914efccb0
Victorodrigues <viictorhgoliveira@gmail.com>
2024-03-04 22:43:25 -0300

    chore: criação de projeto e configuração de tema

---
+++ src/app/index/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+
+import {styles} from "./styles"
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Escolha{"\n"}
+                <Text style={styles.subtitle}>os produtos</Text>
+            </Text>
+            <Text style={styles.subtitle}>Descubra receitas baseadas nos produtos que você escolheu.</Text>
+        </View>
+    )
+}
---
+++ src/app/index/styles.ts
@@ -0,0 +1,19 @@
+import  { StyleSheet } from "react-native"
+
+import {theme} from "@/theme"
+
+export const styles = StyleSheet.create({
+    container:{
+       flex: 1,
+       padding: 24,
+    },
+    title:{
+        fontSize: theme.fonts.size.heading.xl,
+        lineHeight: 44,
+        marginTop: 42,
+        fontFamily: theme.fonts.family.bold
+    },
+    subtitle: {
+        fontFamily: theme.fonts.family.regular
+    },
+});
